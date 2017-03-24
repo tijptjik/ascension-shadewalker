9656be572462ebbf5f605bb68ffb5367e629e01f
Mart van de Ven <m@type.hk>
2017-03-25 01:33:12 +0800

    es6 block

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// GLOBALS
+// ES6 : http://exploringjs.com/es6/ch_core-features.html
 
+{ // BLOCK
+
+let tmp;
 
 // Utilities
-
-var print = function(obj){
+cont print = function(obj){
     console.log(obj)
 }
+
+} // END BLOCK
